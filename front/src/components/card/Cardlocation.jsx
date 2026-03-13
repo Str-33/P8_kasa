91259b87617c27be6c7afe00eb24888acf786971
Esther <ebeaudouin@adoria.com>
2026-03-13 14:23:42 +0100

    fetch logement

--- front/src/components/card/Cardlocation.jsx
+++ front/src/components/card/Cardlocation.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./CardLocation.css";
 
-function CardLocation({ id, title }) {
+function CardLocation({ id, title, cover }) {
   return (
     <Link to={`/location/${id}`}>
-      <div className="card">
+      <div
+        className="card"
+        style={{ backgroundImage: `url(${cover})` }}
+      >
         <p className="card-title">{title}</p>
       </div>
     </Link>
--- front/src/pages/home/home.jsx
+++ front/src/pages/home/home.jsx
@@ -1,25 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CardLocation from "../../components/card/Cardlocation.jsx";
 import Banner from "../../components/banner/Banner.jsx";
 import bannerImg from "../../assets/banner.png";
 import "./home.css";
 
-const locations = [
-  { id: 1, title: "Titre de la location" },
-  { id: 2, title: "Titre de la location" },
-  { id: 3, title: "Titre de la location" },
-  { id: 4, title: "Titre de la location" },
-  { id: 5, title: "Titre de la location" },
-  { id: 6, title: "Titre de la location" },
-];
-
 function Home() {
+  const [logements, setLogements] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8081/api/properties")
+      .then(res => res.json())
+      .then(data => setLogements(data));
+  }, []);
+
   return (
     <>
       <Banner imageSrc={bannerImg} text="Chez nous, partout et ailleurs" />
       <div className="home">
-        {locations.map((loc) => (
-          <CardLocation key={loc.id} id={loc.id} title={loc.title} />
+        {logements.map((logement) => (
+          <CardLocation
+            key={logement.id}
+            id={logement.id}
+            title={logement.title}
+            cover={logement.cover}
+          />
         ))}
       </div>
     </>
