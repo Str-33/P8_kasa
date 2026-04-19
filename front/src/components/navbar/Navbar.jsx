566854f978e4469c938199ccea97d731698cc89d
Esther <ebeaudouin@adoria.com>
2026-04-19 14:27:58 +0200

    Fix : cardlocation; navbar, banner

--- front/src/components/navbar/Navbar.jsx
+++ front/src/components/navbar/Navbar.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "../../assets/logokasa.png";
 import "./Navbar.css";
 
 function Navbar() {
+  const location = useLocation(); 
+
   return (
     <nav className="navbar">
       <img src={logo} alt="Logo Kasa" className="navbar-logo" />
       <div className="navbar-links">
-        <Link to="/">Accueil</Link>
-        <Link to="/About">A Propos</Link>
+        <Link to="/" className={location.pathname === "/" ? "active" : ""}>Accueil</Link>
+        <Link to="/About" className={location.pathname === "/About" ? "active" : ""}>A Propos</Link>
       </div>
     </nav>
   );
